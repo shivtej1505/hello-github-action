558465ca8a2ff1c3b7cfb5243b5c1621c2904e3f
Shivang Nagaria <shivang.nagaria@gmail.com>
2020-05-31 21:43:18 +0530

    v6

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 echo $PWD
 echo $GITHUB_WORKSPACE
-/usr/local/bundle/bin/rubocop --format offenses "$GITHUB_WORKSPACE/"app/services
+/usr/local/bundle/bin/rubocop --format offenses "$GITHUB_WORKSPACE/"app/services > review.txt
+echo review.txt
